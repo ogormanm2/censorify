5699f080369c891ebb9c464b355330b66c8443b9
unknown <matthew.t.ogorman@gmail.com>
2015-09-10 10:01:13 -0400

    first commit

---
+++ censortext.js
@@ -0,0 +1,20 @@
+var censoredWords = ["sad", "bad", "mad"];
+var customCensoredWords = [];
+function censor(inStr) {
+    for (var idx in censoredWords) {
+        inStr = inStr.replace(censoredWords[idx], "****");
+    }
+    for (var idx in customCensoredWords) {
+        inStr = inStr.replace(customCensoredWords[idx], "****");
+    }
+    return inStr;
+}
+function addCensoredWord(word){
+    customCensoredWords.push(word);
+}
+function getCensoredWords(){
+    return censoredWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
